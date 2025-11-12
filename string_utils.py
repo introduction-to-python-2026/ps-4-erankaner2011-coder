4ed7349053760438846406c789f062690ef8c60e
erankaner2011-coder <erankaner2011@gmail.com>
2025-11-12 14:47:09 +0200

    Implement split_before_each_uppercases and split_at_first_digit

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,11 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
-
+    
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digloc = 1
+    for i in formula[1: ]:
+        if digloc.isdigit(i):
+            break
+        digloc += 1
+    return ("digloc")
+        
